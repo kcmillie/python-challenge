96e2090e415ea2ddf4896081c6f6db5ef4a26a8d
ckruder <carmenkchan@gmail.com>
2018-06-20 21:25:07 -0500

    updated main.py

---
+++ PyBank/main.py
@@ -0,0 +1,10 @@
+import csv
+
+# open file
+with open('eggs.csv', newline='') as csvfile:
+	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+# The total number of months included in the dataset
+# The total net amount of "Profit/Losses" over the entire period
+# The average change in "Profit/Losses" between months over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+# The greatest decrease in losses (date and amount) over the entire period
