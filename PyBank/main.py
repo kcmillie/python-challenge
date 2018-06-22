ee854762927f01ebbfbf1fbf96637e77123ad457
ckruder <carmenkchan@gmail.com>
2018-06-22 16:45:40 -0500

    updated main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,12 @@
 import csv
 
 # open file
-with open('eggs.csv', newline='') as csvfile:
-	spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+with open('budget_data.csv', 'rb') as F:
+    reader = csv.reader(F, delimiter=',')
+    for x in reader:
+        print(x)
+        print(x[0])
+        print(x[0], x[1], x[2])
 # The total number of months included in the dataset
 # The total net amount of "Profit/Losses" over the entire period
 # The average change in "Profit/Losses" between months over the entire period
